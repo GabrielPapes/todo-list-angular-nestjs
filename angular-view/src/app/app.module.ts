ff24d12c70b460ed1be59db2375981ba3bbfcca5
GabrielPapes <gabrielpapes@hotmail.com>
2022-08-21 16:59:02 -0300

    style: moving app module stuff to todo module

--- angular-view/src/app/app.module.ts
+++ angular-view/src/app/app.module.ts
@@ -3,70 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { MatSliderModule } from '@angular/material/slider';
-import { DragDropModule } from '@angular/cdk/drag-drop';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatCardModule } from '@angular/material/card';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatListModule } from '@angular/material/list'
-import { MatDividerModule } from '@angular/material/divider';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { HttpClientModule } from '@angular/common/http';
-import { TodoFormComponent } from './components/todo-form/todo-form.component';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatDialogModule } from '@angular/material/dialog';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { StoreModule } from '@ngrx/store';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-
-import { todoReducer } from '../app/states/todo.reducers';
-import { EffectsModule } from '@ngrx/effects';
-import { TodoService } from './services/todo.service';
-import { BoardComponent } from './components/board/board.component';
-import { ListComponent } from './components/list/list.component';
-import { CardComponent } from './components/card/card.component';
+import { TodosModule } from './components/todos/todos.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoFormComponent,
-    BoardComponent,
-    ListComponent,
-    CardComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    MatSliderModule,
-    DragDropModule,
-    MatGridListModule,
-    MatCardModule,
-    MatMenuModule,
-    MatIconModule,
-    MatButtonModule,
-    LayoutModule,
-    MatListModule,
-    MatDividerModule,
-    MatToolbarModule,
-    HttpClientModule,
-    MatFormFieldModule,
-    MatInputModule, 
-    MatDialogModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MatButtonToggleModule,
-    StoreModule.forRoot({todos: todoReducer}),
-    EffectsModule.forRoot([TodoService]),
-    MatCheckboxModule,
-    
+    TodosModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ angular-view/src/app/components/todos/todos.module.ts
@@ -0,0 +1,72 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatSliderModule } from '@angular/material/slider';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatListModule } from '@angular/material/list'
+import { MatDividerModule } from '@angular/material/divider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { HttpClientModule } from '@angular/common/http';
+import { TodoFormComponent } from './todo-form/todo-form.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDialogModule } from '@angular/material/dialog';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { StoreModule } from '@ngrx/store';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
+import { todoReducer } from '../../states/todo.reducers';
+import { EffectsModule } from '@ngrx/effects';
+import { TodoService } from '../../services/todo.service';
+import { BoardComponent } from './board/board.component';
+import { ListComponent } from './list/list.component';
+import { CardComponent } from './card/card.component';
+
+@NgModule({
+  declarations: [
+    TodoFormComponent,
+    BoardComponent,
+    ListComponent,
+    CardComponent,
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    DragDropModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    LayoutModule,
+    MatListModule,
+    MatDividerModule,
+    MatToolbarModule,
+    HttpClientModule,
+    MatFormFieldModule,
+    MatInputModule, 
+    MatDialogModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonToggleModule,
+    StoreModule.forRoot({todos: todoReducer}),
+    EffectsModule.forRoot([TodoService]),
+    MatCheckboxModule,
+    
+  ],
+  exports: [
+    BoardComponent,
+  ],
+  providers: [],
+})
+export class TodosModule { }
