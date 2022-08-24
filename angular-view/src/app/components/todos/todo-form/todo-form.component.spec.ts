7566aaaf0750284fbd45d7871da73407ed9aa39d
GabrielPapes <gabrielpapes@hotmail.com>
2022-08-24 12:38:42 -0300

    test: todo-form component

--- angular-view/src/app/components/todos/todo-form/todo-form.component.spec.ts
+++ angular-view/src/app/components/todos/todo-form/todo-form.component.spec.ts
@@ -1,8 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { MatDialogRef } from '@angular/material/dialog';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Actions } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
+import { SubTodo } from 'src/app/models/subtodo.model';
 import { TodoFormComponent } from './todo-form.component';
 import ActionWithPayload from 'src/app/models/actionWithPayload.model';
 import { Todo } from 'src/app/models/todo.model';
@@ -13,17 +14,21 @@ describe('TodoFormComponent', () => {
 
   beforeEach(() => {
     const matDialogRefStub = () => ({ close: () => ({}) });
+
+    // TODO improving this stub to verify ofType and takeUntil if possible.
     const actionsStub = () => ({
       pipe: (arg: any, arg1: any) => ({ subscribe: (f: any) => f({}) })
     });
+    
     const storeStub = () => ({ dispatch: (todoAction: ActionWithPayload<Todo>) => ({}) });
     TestBed.configureTestingModule({
       schemas: [NO_ERRORS_SCHEMA],
       declarations: [TodoFormComponent],
       providers: [
-        { provide: MatDialogRef, useFactory: matDialogRefStub },
         { provide: Actions, useFactory: actionsStub },
-        { provide: Store, useFactory: storeStub }
+        { provide: Store, useFactory: storeStub },
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+        { provide: MatDialogRef, useFactory: matDialogRefStub },
       ]
     });
     fixture = TestBed.createComponent(TodoFormComponent);
@@ -34,8 +39,26 @@ describe('TodoFormComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  it(`subtodos has default value`, () => {
+    expect(component.subtodos).toEqual([]);
+  });
+
   it(`toggleEdit has default value`, () => {
     expect(component.toggleEdit).toEqual(false);
+  });
+
+  it(`addSubtodo has default value`, () => {
+    expect(component.addSubtodo).toEqual(false);
+  });
+
+  describe('onDeleteSubtodo', () => {
+    it('makes expected calls', () => {
+      const storeStub: Store = fixture.debugElement.injector.get(Store);
+      const subTodoStub: SubTodo = <any>{};
+      spyOn(storeStub, 'dispatch').and.callThrough();
+      component.onDeleteSubtodo(subTodoStub);
+      expect(storeStub.dispatch).toHaveBeenCalled();
+    });
   });
 
   describe('onSubmit', () => {
@@ -49,7 +72,7 @@ describe('TodoFormComponent', () => {
 
   describe('onDelete', () => {
     it('makes expected calls', () => {
-      const matDialogRefStub: MatDialogRef = fixture.debugElement.injector.get(
+      const matDialogRefStub: MatDialogRef<TodoFormComponent> = fixture.debugElement.injector.get(
         MatDialogRef
       );
       const storeStub: Store = fixture.debugElement.injector.get(Store);
